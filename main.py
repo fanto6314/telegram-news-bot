32a17bc0170f3be07d6804a4b06478030c4d5c8e
Antonio <62208053+fanto6314@users.noreply.github.com>
2022-03-03 20:06:33 +0100

    [+] Updated news()
    TODO: Fix news()

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import telegram.ext
 import requests
+import json
 
 with open ('token.txt', 'r') as f:
     token = f.read()
 
 api = "df6a9ccf303ac1478b7155b55d0c3c33"
 languages = "en"
+limit = "5"
+keywords = ""
 
-api_url = "http://api.mediastack.com/v1/news?access_key=" + api + "&languages=" + languages
+api_url = "http://api.mediastack.com/v1/news?access_key=" + api + "&languages=" + languages + "&limit=" + limit + "&keywords=" + keywords
 
 def start(update, context):
     update.message.reply_text('Hello, I am PythonWorldNews_bot!')
@@ -15,18 +18,35 @@ def start(update, context):
 def help(update, context):
     update.message.reply_text("""
     Available commands:
-
     /start - start bot
     /help - help
+    /news - get news
     """)
 
-def getNews():
-    response = requests.get(api_url)
-    print(response.json())
-
+#TODO: Fix this half-woking shit
 def news(update, context):
-    update.message.reply_text("This is news command")
+    limit = 5
+    response = requests.get(api_url)
+    json_data = json.loads(response.text)
+    for i in range(0, limit):
+        #update.message.reply_text(json_data["data"][i]["image"] + json_data["data"][i]["title"])
+        title = json_data["data"][i]["title"]
+        description = json_data["data"][i]["description"]
+        author = json_data["data"][i]["author"]
+        image = json_data["data"][i]["image"]
 
+        caption = "<a>" + title + "\n" + description + "\n Author: " + author + "</a>"
+        chat_id = update.message.chat_id
+
+        context.bot.send_photo(
+            chat_id=chat_id,
+            photo=image,
+            caption=caption,
+            parse_mode=telegram.ParseMode.HTML
+        )
+
+
+# -------------------------   Main   ------------------------- #
 if __name__ == '__main__':
     updater = telegram.ext.Updater(token, use_context=True)
     dispatcher = updater.dispatcher
@@ -35,9 +55,7 @@ if __name__ == '__main__':
     dispatcher.add_handler(telegram.ext.CommandHandler('help', help))
     dispatcher.add_handler(telegram.ext.CommandHandler('news', news))
 
-    getNews()
-
-    updater.start_polling()
-    print("Bot started successfully!")
-    updater.idle()
-    print("Bot stopped")
+    if updater.start_polling():
+        print("Bot started successfully!")
+    if updater.idle():
+        print("Bot stopped")
