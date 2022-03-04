da38f07f4d2b0398cefdd0a4e2e8e7e97135e19d
Antonio <62208053+fanto6314@users.noreply.github.com>
2022-03-04 15:35:44 +0100

    [~] Small fix to news()
    [~] Still need to fix start(), currentrly not working well...

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ with open ('token.txt', 'r') as f:
 
 api = "df6a9ccf303ac1478b7155b55d0c3c33"
 languages = "en"
-limit = "5"
-keywords = ""
+limit = "1"
+newsTimer = 10
 
-api_url = "http://api.mediastack.com/v1/news?access_key=" + api + "&languages=" + languages + "&limit=" + limit + "&keywords=" + keywords
+api_url = "http://api.mediastack.com/v1/news?access_key=" + api + "&languages=" + languages + "&limit=" + limit
 
+#TODO: fix send news every x minutes not working
 def start(update, context):
     update.message.reply_text('Hello, I am PythonWorldNews_bot!')
+    context.job_queue.run_repeating(news(update, context), interval=newsTimer, first=0)
 
 def help(update, context):
     update.message.reply_text("""
@@ -23,28 +25,31 @@ def help(update, context):
     /news - get news
     """)
 
-#TODO: Fix this half-woking shit
 def news(update, context):
-    limit = 5
+    limit = 1
     response = requests.get(api_url)
     json_data = json.loads(response.text)
     for i in range(0, limit):
         #update.message.reply_text(json_data["data"][i]["image"] + json_data["data"][i]["title"])
-        title = json_data["data"][i]["title"]
-        description = json_data["data"][i]["description"]
-        author = json_data["data"][i]["author"]
-        image = json_data["data"][i]["image"]
+        if(json_data["data"][i]["image"]):
+            caption = "<a>" + json_data["data"][i]["title"] + "\n" + json_data["data"][i]["description"] + "\n Author: " + json_data["data"][i]["author"] + "</a>"
+            chat_id = update.message.chat_id
 
-        caption = "<a>" + title + "\n" + description + "\n Author: " + author + "</a>"
-        chat_id = update.message.chat_id
+            context.bot.send_photo(
+                chat_id=chat_id,
+                photo=json_data["data"][i]["image"],
+                caption=caption,
+                parse_mode=telegram.ParseMode.HTML
+            )
+        else:
+            caption = "<a>" + json_data["data"][i]["title"] + "\n" + json_data["data"][i]["description"] + "\n Author: " + json_data["data"][i]["author"] + "</a>"
+            chat_id = update.message.chat_id
 
-        context.bot.send_photo(
-            chat_id=chat_id,
-            photo=image,
-            caption=caption,
-            parse_mode=telegram.ParseMode.HTML
-        )
-
+            context.bot.send_message(
+                chat_id=chat_id,
+                text=caption,
+                parse_mode=telegram.ParseMode.HTML
+            )
 
 # -------------------------   Main   ------------------------- #
 if __name__ == '__main__':
@@ -53,7 +58,6 @@ if __name__ == '__main__':
 
     dispatcher.add_handler(telegram.ext.CommandHandler('start', start))
     dispatcher.add_handler(telegram.ext.CommandHandler('help', help))
-    dispatcher.add_handler(telegram.ext.CommandHandler('news', news))
 
     if updater.start_polling():
         print("Bot started successfully!")
